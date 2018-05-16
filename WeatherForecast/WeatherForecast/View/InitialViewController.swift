6a25aba304cb5ea91c74e5f686c90b723daddb89
John <ionut_popovici92@yahoo.com>
2018-05-16 11:26:10 +0300

    Added WeatherForecast storyboards

---
+++ WeatherForecast/WeatherForecast/View/InitialViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by John on 5/11/18.
+//  Copyright © 2018 John. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+}
